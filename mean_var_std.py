6ecff0e8af2857479e4fa74be7a9dd7a3e8a2673
Eli Reed <elireed1414@gmail.com>
2024-12-25 16:25:07 +0000

    STarting project

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,13 @@
 import numpy as np
 
 def calculate(list):
-
-
+    matrix = np.array(list).reshape(3, 3)
+    flatMatrix = matrix.flatten()
+    means = np.array([matrix.mean(axis=0), matrix.mean(axis=1), flatMatrix.mean()])
+    stDevs = np.array([matrix.stdev(axis=0), matirx.stdev(axis=1), flatMatrix.stdev()])
+    variances = np.array([matrix.var(axis=0), matrix.var(axis=1), flatMatrix.var()])
+    mins = np.array([matrix.min(axis=0), matrix.min(axis=1), flatMatrix.min()])
+    maxs = np.array([matrix.max(axis=0), matrix.max(axis=1), flatMatrix.max()])
 
 
     return calculations
